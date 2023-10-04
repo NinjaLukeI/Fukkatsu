ef0f36f1a736d00d5c428f7557da5be54e1e09f1
Luke Ibeachum <luke_ibeachum@hotmail.co.uk>
2023-10-04 22:08:18 +0100

    buttons to reader overlay

--- Fukkatsu/Views/readerOverlay.swift
+++ Fukkatsu/Views/readerOverlay.swift
@@ -10,31 +10,61 @@ import SwiftUI
 struct readerOverlay: View {
     var body: some View {
         
-        
-        GeometryReader{ proxy in
-            RoundedRectangle(cornerRadius: 15)
-                .overlay(
-                    HStack{
-                        VStack(alignment: .leading){
-                            Text("manga name").font(.headline).foregroundColor(.white)
-                                
-                            
-                            Text("Chapter Name").font(.caption).foregroundColor(.white)
-                        }
-                        
-                        Spacer()
-                        
-                        Text("Exit").foregroundColor(.red)
-                    }.frame(width: proxy.size.width - 100)
-                        
-                        
-                        
-                ).padding(.horizontal, 30.0)
-                .frame(width: proxy.size.width , height: 60)
+        VStack{
             
+            //the proxy inherits the size of the parent view which in this case is just the screen
+            GeometryReader{ proxy in
+                RoundedRectangle(cornerRadius: 15)
+                    .overlay(
+                        HStack{
+                            VStack(alignment: .leading){
+                                Text("manga name").font(.headline).foregroundColor(.white)
+                                    .lineLimit(1)
+                                    
+                                    
+                                Text("Chapter Name").font(.caption).foregroundColor(.white)
+                                    .lineLimit(1)
+                            }
+                            
+                            Spacer()
+                            
+                            Text("Exit").foregroundColor(.red)
+                        }.frame(width: proxy.size.width - 100)
+                            
+                            
+                            
+                    ).padding(.horizontal, 30.0)
+                    .frame(width: proxy.size.width , height: 60)
+                    
+            }
+            
+            HStack{
+                Button {
+                    
+                } label: {
+                    Image(systemName: "highlighter")
+                        .frame(width: 28, height: 28)
+                        .background(.black)
+                        .cornerRadius(15)
+                        .foregroundColor(.white)
+                        
+                }
                 
-                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "highlighter")
+                        .frame(width: 28, height: 28)
+                        .background(.black)
+                        .cornerRadius(15)
+                        .foregroundColor(.white)
+                        
+                }
+
+            }
+            
         }
+        
         
     }
 }
